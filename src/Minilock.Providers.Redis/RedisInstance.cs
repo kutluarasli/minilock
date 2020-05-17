4d3bd0ee84638396f92dfa65f24c73ee66124f24
Kutlu Araslı <kutlu.arasli@trendyol.com>
2020-05-17 22:00:53 +0300

    Change port variable type to int32

--- src/Minilock.Providers.Redis/RedisInstance.cs
+++ src/Minilock.Providers.Redis/RedisInstance.cs
@@ -5,19 +5,19 @@ namespace Minilock.Providers.Redis
     public class RedisInstance
     {
         public string Address { get; }
-        public ushort Port { get; }
+        public int Port { get; }
         public string AuthKey { get;}
         
         public string FullAddress =>  $"{Address}:{Port}";
 
-        public RedisInstance(string address, ushort port = 6379, string authKey = null)
+        public RedisInstance(string address, int port = 6379, string authKey = null)
         {
             if (string.IsNullOrWhiteSpace(address))
             {
                 throw new ArgumentNullException(nameof(address));
             }
 
-            if (port == 1)
+            if (port <= 0)
             {
                 throw new ArgumentException(nameof(port));
             }
